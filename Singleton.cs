89847741d12bd20ac8af0eca1ebe08593a99524d
VladimirRepp <57412538+VladimirRepp@users.noreply.github.com>
2023-09-20 12:46:15 +0700

    Add Singleton
    Generic Singleton MonoBehaviour

---
+++ Singleton.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T : Component
+{
+    private static T INSTANCE;
+
+    public static T Instance
+    {
+        get
+        {
+            if (INSTANCE == null)
+            {
+                INSTANCE = FindObjectOfType<T>();
+
+                if (INSTANCE == null)
+                {
+                    GameObject obj = new GameObject();
+                    obj.name = typeof(T).Name;
+                    INSTANCE = obj.AddComponent<T>();
+                }
+            }
+            return INSTANCE;
+        }
+    }
+
+    public virtual void Awake()
+    {
+        if (INSTANCE == null)
+        {
+            INSTANCE = this as T;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
