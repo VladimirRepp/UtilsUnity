edb5e111a1c87afbd41fbf84a9a74513ffa95457
VladimirRepp <57412538+VladimirRepp@users.noreply.github.com>
2023-09-20 12:20:50 +0700

    Add Utilities
    Utilities static class

---
+++ Utilities.cs
@@ -0,0 +1,20 @@
+using System.Xml.Serialization;
+using System.IO;
+
+public static class Utilities 
+{
+    public static string Serialize<T>(this T toSerialize)
+    {
+        XmlSerializer xml = new XmlSerializer(typeof(T));
+        StringWriter writer = new StringWriter();
+        xml.Serialize(writer, toSerialize);
+        return writer.ToString();
+    }
+
+    public static T Deserialize<T>(this string toDeserialize)
+    {
+        XmlSerializer xml = new XmlSerializer(typeof(T));
+        StringReader reader = new StringReader(toDeserialize);
+        return (T)xml.Deserialize(reader);
+    }
+}
