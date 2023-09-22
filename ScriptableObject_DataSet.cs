d699277caf48654193a0a3930561a3b1adb58e99
VladimirRepp <57412538+VladimirRepp@users.noreply.github.com>
2023-09-22 16:14:36 +0700

    Add ScriptableObject_DataSet
    List Template ScriptableObject

---
+++ ScriptableObject_DataSet.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptableObject_DataSet<T> : ScriptableObject 
+{
+    public List<T> _data;
+
+    public List<T> Data
+    {
+        get => _data;
+        set => _data = value;
+    }
+    public T this[int index]
+    {
+        get => _data[index];
+        set => _data[index] = value;
+    }
+    public int Count => _data.Count;
+
+    public void Add(T d)
+    {
+        _data.Add(d);
+    }
+
+    public void Remove(T d)
+    {
+        _data.Remove(d);
+    }
+
+    public void RemoveAt(int index)
+    {
+        _data.RemoveAt(index);
+    }
+}
